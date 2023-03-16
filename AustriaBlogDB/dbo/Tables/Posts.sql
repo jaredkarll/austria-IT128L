c96144b072713807c6b8b5f2e0415180bef0bfb8
Jared <98764191+jaredkarll@users.noreply.github.com>
2023-03-16 21:07:11 +1000

    LE4 Activity

--- AustriaBlogDB/dbo/Tables/Posts.sql
+++ AustriaBlogDB/dbo/Tables/Posts.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [UserId] INT NOT NULL, 
-    [Title] NVARCHAR(10) NOT NULL, 
+    [Title] NVARCHAR(150) NOT NULL, 
     [Body] TEXT NOT NULL, 
     [DateCreated] DATETIME2 NOT NULL, 
     CONSTRAINT [FK_Posts_Users] FOREIGN KEY (UserId) REFERENCES Users(Id)
